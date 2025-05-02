3370f50560e54bf4a9c4caa90d94b7bb2675aa94
cmdBlack <kjjmacni@gmail.com>
2025-05-02 18:26:06 +0800

    Added UTC script for UTC data where data of a date ranges from 8:10 AM today to 8:00 AM tomorrow

--- monthly_sort_utc.py
+++ monthly_sort_utc.py
@@ -19,12 +19,16 @@ data = pd.read_csv("outputs/" + file_path)
 frame = pd.DataFrame(data)
 frame["date"] = frame["logtime"].str.split(pat=" ",n=1,expand=True)[0]
 frame["time"] = frame["logtime"].str.split(pat=" ",n=1,expand=True)[1]
+frame["timestamp"] = pd.to_datetime(frame["logtime"])
+frame["timestamp"] = frame["timestamp"] - pd.to_timedelta(8, unit='h')
 
-csv_file_set = set()
+csv_file_set_utc = set()
 
+# UTC
 for row in tqdm(range(len(frame.recno))):
-    csv_file = pathlib.Path("outputs/" + "monthly-" + frame.loc[row]["date"][:7] + "-" + station + ".csv")
-    csv_file_set.add(csv_file)
+    csv_file = pathlib.Path(
+        "outputs/" + "monthly-" + str(frame.loc[row]["timestamp"])[:7] + "-" + station + "-utc" + ".csv")
+    csv_file_set_utc.add(csv_file)
 
     datarow = frame.loc[row].to_frame().T
     # datarow.to_csv(str(frame.loc[row-1]["nodeid"]) + "-" +  str(frame.loc[row-1]["logtype"]) + ".csv", mode='a', header=False)
@@ -32,11 +36,12 @@ for row in tqdm(range(len(frame.recno))):
 
 #print("DONE")
 
-for csv in tqdm(csv_file_set):
+# UTC
+for csv in tqdm(csv_file_set_utc):
     data_prev = pd.read_csv(csv)
     frame2 = pd.DataFrame(data_prev)
 
-    frame2["timestamp"] = pd.to_datetime(frame2["logtime"])
+    frame2["timestamp"] = pd.to_datetime(frame2["timestamp"])
     frame2.set_index('timestamp', inplace=True)
     frame3 = frame2.copy()
     del frame2["recno"]
@@ -69,7 +74,7 @@ for csv in tqdm(csv_file_set):
         frame2['time'] = [item.replace('00:00:00', '24:00:00') for item in frame2['time']]
 
         frame4 = frame2.pivot_table(index='time', columns='date', values='value', aggfunc='first')
-        frame4.to_csv(pathlib.Path(str(csv).replace('monthly-', 'monthly-table-')))
+        frame4.to_csv(pathlib.Path(str(csv).replace('monthly-', 'utc-monthly-table-')))
     else:
         # del frame2["value"]
         # frame3 = frame2.pivot(index='time', columns='date', values='wl_data')
@@ -80,10 +85,9 @@ for csv in tqdm(csv_file_set):
         frame2['time'] = [item.replace('00:00:00', '24:00:00') for item in frame2['time']]
 
         frame4 = frame2.pivot_table(index='time', columns='date', values='wl_data', aggfunc='first')
-        frame4.to_csv(pathlib.Path(str(csv).replace('monthly-', 'monthly-table-')))
+        frame4.to_csv(pathlib.Path(str(csv).replace('monthly-', 'utc-monthly-table-')))
+
 print('DONE')
-
-
 
 """
 for csv in tqdm(csv_file_set):
